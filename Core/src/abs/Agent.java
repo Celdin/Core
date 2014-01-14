667d08ad62e39134ef6a3e0a130b6e69f6cb7462
Celdin <Sylvain.goulliart@gmail.com>
2014-01-14 17:42:43 +0100

    Mise en place. Plus d'environement abstrait.

--- Core/src/abs/Agent.java
+++ Core/src/abs/Agent.java
@@ -1,5 +1,15 @@
 package abs;
 
-public interface Agent {
-	public void run(Environement environement);
+import impl.Univert;
+
+public abstract class Agent {
+	private int pos_x = -1;
+	private int pos_y = -1;
+	
+	public Agent(int x,int y){
+		pos_x=x;
+		pos_y=y;
+	}
+	
+	public abstract void run(Univert univert);
 }
--- Core/src/impl/Univert.java
+++ Core/src/impl/Univert.java
@@ -1,11 +1,17 @@
 package impl;
 
 import abs.Agent;
-import abs.Environement;
 
-public class Univert implements Environement{
+public class Univert{
+	
 	public Agent grille[][];
+	public Boolean torique = false;
+
 	Univert(int x,int y){
 		grille = new Agent[x][y];
+	}
+	Univert(int x,int y,Boolean torique){
+		grille = new Agent[x][y];
+		this.torique=torique;
 	}
 }
