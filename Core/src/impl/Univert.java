66781e1e703ffe833134fe92c2a2923d1e3176c0
Celdin <Sylvain.goulliart@gmail.com>
2014-01-14 17:49:46 +0100

    Y'as tout

--- Core/src/impl/Univert.java
+++ Core/src/impl/Univert.java
@@ -4,10 +4,14 @@ import abs.Agent;
 
 public class Univert{
 	
+	public int max_x;
+	public int max_y;
 	public Agent grille[][];
 	public Boolean torique = false;
 
 	Univert(int x,int y){
+		max_x = x;
+		max_y = y;
 		grille = new Agent[x][y];
 	}
 	Univert(int x,int y,Boolean torique){
