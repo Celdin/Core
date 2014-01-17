bfe038aa0f5911f3193b0e45aa17b0565f18c1ea
Celdin <Sylvain.goulliart@gmail.com>
2014-01-17 15:55:52 +0100

    Plus de valeur

--- Core/src/Main.java
+++ Core/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException, IOException {
 		SMA sma = new SMA(new Environment(200,2,50,10,3, 31, -1, -1, 5,true),new ArrayList<Agent>());
 		sma.addAgent();
-		sma.run(1000);
+		sma.run(2000);
 	}
 
 }
